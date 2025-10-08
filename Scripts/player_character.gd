62cbd660b19690c07b360a019b90fc2bfc46b8d6
Ray Deaver <raymondmichael58@gmail.com>
2025-10-08 10:12:05 -0500

    Edited scripts for player

---
+++ Scripts/player_character.gd
@@ -0,0 +1,13 @@
+extends CharacterBody2D
+
+const SPEED = 300.0
+
+func _physics_process(delta: float) -> void:
+	#Movement left, right
+	var direction := Input.get_axis("move_left", "move_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
